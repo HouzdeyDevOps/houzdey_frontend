82d9a8c099fd4a559b0f325e848a8024cbb09e93
el-collins <charlescollins476@gmail.com>
2025-01-28 23:22:17 +0100

    BUILD FIXES

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import HomePage from "./(pages)/(properties)/page";
 
+
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  experimental: {
+    optimizeCss: false,
+  },
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
 };
 
 export default nextConfig;
